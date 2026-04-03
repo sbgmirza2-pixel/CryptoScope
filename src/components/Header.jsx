8d28008bdaf15b0c6f01468bf8156bdbb2d6d85f
sbgmirza@-pixel <sbgmirza2@gmail.com>
2026-04-03 18:20:15 +0500

    message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-gradient-to-r from-purple-900 to-indigo-500 p-6 text-center rounded-b-lg shadow-md">
+    <header className="bg-purple-900 to-indigo-500 p-6 text-center rounded-b-lg shadow-md">
       <h1 className="text-white text-3xl font-bold">CryptoPulse</h1>
       <p className="text-purple-200 mt-1">Live Crypto Price Tracker</p>
     </header>
